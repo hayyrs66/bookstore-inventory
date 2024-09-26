f44cad816661536e850d36f57e202d8d4223e875
Your Name <rayrtsx@proton.me>
2024-09-25 22:04:36 -0600

    change paths

--- books.cpp
+++ books.cpp
@@ -2,9 +2,8 @@
 #include "Testing.h"
 
 int main() {
-	
 	Testing test(20);
-	test.uploadBooks("./compress/lab01_books.csv");
-	test.executeSearch("./compress/lab01_search.csv", "output.txt");
+	test.uploadBooks("./compress/100K/100Klab01_books.csv");
+	test.executeSearch("./compress/100K/100Klab01_search.csv", "output.txt");
 	return 0;
 }
