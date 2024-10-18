937e2d19e9dc09f93bcd6ec2b42a296dde930268
Your Name <rayrtsx@proton.me>
2024-10-18 16:41:36 -0600

    Change file paths in books.cpp

--- books.cpp
+++ books.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	Testing test(20);
-	test.uploadBooks("./compress/100K/100Klab01_books.csv");
-	test.executeSearch("./compress/100K/100Klab01_search.csv", "output.txt");
+	test.uploadBooks("100Klab01_books.csv");
+	test.executeSearch("100Klab01_search.csv", "new_output.txt" ,"new_output.txt.enc", "ok:uo1IN");
 	return 0;
 }
